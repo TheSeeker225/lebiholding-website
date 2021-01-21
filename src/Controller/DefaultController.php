4a099571e45b492a9fb78caae7fe759a73c4f3f2
Wilfried YORO <wilfriedyoro68@gmail.com>
2021-01-21 13:42:42 +0000

    Creation des pages du site web - Partie 1 - terminee

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -15,4 +15,28 @@ class DefaultController extends AbstractController
     {
         return $this->render('default/index.html.twig', []);
     }
+
+    /**
+     * @Route("/nos-prestations", name="terms_conditions")
+     */
+    public function getServices(): Response
+    {
+        return $this->render('default/services.html.twig', []);
+    }
+
+    /**
+     * @Route("/termes-et-conditions", name="terms_conditions")
+     */
+    public function getTerms(): Response
+    {
+        return $this->render('default/terms-and-conditions.html.twig', []);
+    }
+
+    /**
+     * @Route("/mon-profil", name="user_account")
+     */
+    public function getProfile(): Response
+    {
+        return $this->render('default/account-profile.html.twig', []);
+    }
 }
